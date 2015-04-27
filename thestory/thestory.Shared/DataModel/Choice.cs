41f2fdf26fcec36531e7aa4968f96d368dd658b9
Ferdinand <Ferdinand@localhost>
2015-04-27 14:34:41 +0200

    dataservice, fetching of initial story

---
+++ thestory/thestory.Shared/DataModel/Choice.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thestory.DataModel
+{
+    public class Choice
+    {
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "ancestorId")]
+        public string AncestorId { get; set; }
+
+        [JsonProperty(PropertyName = "choiceId")]
+        public string ChoiceId { get; set; }
+    }
+}
--- thestory/thestory.Shared/DataModel/StoryItem.cs
+++ thestory/thestory.Shared/DataModel/StoryItem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace thestory
+namespace thestory.DataModel
 {
     public class StoryItem
     {
@@ -12,13 +12,19 @@ namespace thestory
         [JsonProperty(PropertyName = "text")]
         public string Text { get; set; }
 
-        [JsonProperty(PropertyName = "choice")]
-        public string Choice { get; set; }
+        [JsonProperty(PropertyName = "isroot")]
+        public bool IsRoot { get; set; }
 
         [JsonProperty(PropertyName = "language")]
         public string Language { get; set; }
 
         [JsonProperty(PropertyName = "popularity")]
         public int Popularity { get; set; }
+
+        [JsonProperty(PropertyName = "choice")]
+        public string Choice { get; set; }
+
+        [JsonIgnore]
+        public List<Choice> Choices { get; set; }
     }
 }
